35fb2c8b5c6b82b73f0929c2fa756c5c73a9846e
scandyna <scandyna@78c54d24-660f-0410-8d2e-dca26f247d32>
2006-07-26 21:51:21 +0000

    Works on peack meter. I think this comes to a working state. I made some tests with 0dB 1kHz sinus, and the meter shows 0dB - fine. Another test with a dB-meter shows I have an offset (have HW mixers on soundcard), but values are Ok. But, compare to a standard PPM (don't have a real DPM, rrr!), the meter is wrong. (to high). So, later I must clean the code, do somthing for graphics.

--- trunk/src/lp_peackmeter/lp_peackmeter.cpp
+++ trunk/src/lp_peackmeter/lp_peackmeter.cpp
@@ -31,6 +31,11 @@ lp_peackmeter::lp_peackmeter(QWidget *parent, const char *name, int nb_channels,
 
 	// Default clip hold time
 	pv_clip_timeout = 200000;
+	// Initialize clip system...
+	int i;
+	for(i=0; i<pv_channels; i++){
+		pv_start_clip(i);
+	}
 
 	// Memory for buffer
 	if(buf_size <= 0){
@@ -46,11 +51,10 @@ lp_peackmeter::lp_peackmeter(QWidget *parent, const char *name, int nb_channels,
 	}
 
 	// FIXME:
-	// I have several performance problem. With low sample_time, the clipping
-	// detection works not. The value here is hazardous
+	// I have several performance problem. With low sample_time. The value here is hazardous
 	pv_minimal_sample_time = 1; // AMD Athlon 2Ghz
 
-	// Get a dlg instance
+	// Get the dlg instance
 	pv_peack_meter_dlg = new lp_peackmeter_dlg(parent, name, this);
 
 	// Start the thread
@@ -64,6 +68,8 @@ lp_peackmeter::~lp_peackmeter()
 	}
 }
 
+// Copy the samples so fast as possible and return
+// The display is then performed from other threads
 int lp_peackmeter::run_buffer(float *buffer, int len)
 {
 	// Check
@@ -81,8 +87,6 @@ int lp_peackmeter::run_buffer(float *buffer, int len)
 		std::cerr << "lp_peackmeter::run_buffer: buffer len is bigger than alocated memory\n";
 		return -1;
 	}
-
-	//std::cout << "lp_peackmeter: recus " << len << " samples\n";
 
 	int i;
 	QMutexLocker locker(&pv_mutex);
@@ -105,20 +109,22 @@ int lp_peackmeter::get_channels()
 	return pv_channels;
 }
 
+// Calcualte the time to wait after each sample
 int lp_peackmeter::set_samplerate(int samplerate)
 {
-	// Sampletime
-	double d_tmp1 = 0, d_tmp2 = 0;
+	QMutexLocker locker(&pv_mutex);
+
 	// Verification
 	if(samplerate == 0) {
 		std::cerr << "lp_peackmeter::lp_peackmeter: samplerate is Null\n";
 		return -1;
 	}
+	// Sampletime
+	double d_tmp1 = 0, d_tmp2 = 0;
 	// Set the sample time
 	d_tmp1 = (double)samplerate;
 	d_tmp2 = ((1 / (d_tmp1 * (double)pv_channels)) * 1000000);
 	d_tmp2 = round(d_tmp2);
-	//pv_sample_time = (unsigned int)((1 / (samplerate * pv_channels)) * 1000000);
 	pv_sample_time = (unsigned int)d_tmp2;
 	std::cout << "Sample time: " << (unsigned int)d_tmp2 << std::endl;
 
@@ -127,7 +133,9 @@ int lp_peackmeter::set_samplerate(int samplerate)
 
 // Protected
 
-// NOTE: runs mono for now !!
+// The "fast" thread: no graphical displaying here
+// only peack are stored and clip detection is 
+// performed here
 void lp_peackmeter::run()
 {
 	while(1){
@@ -135,27 +143,19 @@ void lp_peackmeter::run()
 		pv_qwc.wait();
 		pv_running = TRUE;
 		pv_buf_end = FALSE;
-		float tmp;
+		//float tmp;
 
 		int i, j;
 		for(i=0; i<(pv_buf_len/pv_channels); i++) {
 			for(j=0; j<pv_channels; j++){
-				tmp = pv_buffer[i*pv_channels+j];
-				//pv_val[j] = tmp;
-	
+
 				// Set negative to positive
-				if(tmp < 0){
-					tmp = -tmp;
+				if(pv_buffer[i*pv_channels+j] < 0){
+					pv_buffer[i*pv_channels+j] = -pv_buffer[i*pv_channels+j];
 				}
 
-				pv_val[j] = tmp;
-	
-				// Get absolute vaue
-				//pv_val = abs((float)pv_val);
-	
-				// Get RMS value
-				//pv_val = sqrt((float)pv_val);
-	
+				pv_val[j] = pv_buffer[i*pv_channels+j];
+		
 				// Keep buffer peack
 				if(pv_val[j] > pv_last_val[j]){
 					pv_last_val[j] = pv_val[j];
@@ -169,11 +169,9 @@ void lp_peackmeter::run()
 				// Clip detection
 				if(pv_val[j] >= pv_range_high){
 					pv_val[j] = pv_range_high;
-				//	std::cout << "Clip decteted ! ****\n";
-					pv_start_clip();
+					pv_start_clip(j);
 				}
 			}
-			//std::cout << "Sleeping for " << pv_sample_time << "\n";
 			usleep(pv_sample_time);
 			// If we are to slow
 			if(pv_buf_end == TRUE){
@@ -185,61 +183,65 @@ void lp_peackmeter::run()
 				break;
 			}
 		}
-		//std::cout << "Peak metre: Nbre de samples traites: " << i << std::endl;
 		pv_running = FALSE;
 	}
 }
 
-bool lp_peackmeter::get_clip()
+bool lp_peackmeter::get_clip(int channel)
 {
 	QMutexLocker locker(&pv_dlg_mutex);
 
-/*	if(pv_clip_is_timeout() == TRUE){
-		std::cout << "Clip is timeout\n";
+	if(channel >= pv_channels){
+		std::cerr << "lp_peackmeter::get_clip: channel value is to high\n";
+		return -1;
 	}
-	if(pv_clip_is_timeout() == FALSE){
-		std::cout << "Clip is NOT timeout\n";
-	}
-	if(pv_clip_to_show == TRUE){
-		std::cout << "Clip is to show\n";
-	}
-	if(pv_clip_to_show == FALSE){
-		std::cout << "Clip is  NOT to show\n";
-	}
-*/
-	if((pv_clip_is_timeout() == FALSE)||(pv_clip_to_show == TRUE)) {
-		pv_clip_to_show = FALSE;
+
+	if((pv_clip_is_timeout(channel) == FALSE)||(pv_clip_to_show[channel] == TRUE)) {
+		pv_clip_to_show[channel] = FALSE;
 		return TRUE;
 	}else{
 		return FALSE;
 	}
 }
 
-float lp_peackmeter::get_value()
+// Pass the actuel peack value and reset
+float lp_peackmeter::get_value(int channel)
 {
 	QMutexLocker locker(&pv_dlg_mutex);
-	float tmp;
-	tmp = pv_last_val[0];
-	//pv_val[0] = pv_last_val[0];
-	pv_last_val[0] = 0;
-	//return pv_val[0];
-	return tmp;
+	if(channel >= pv_channels){
+		std::cerr << "lp_peackmeter::get_value: channel value is to high\n";
+		return -1;
+	}
+
+	pv_val[channel] = pv_last_val[channel];
+	pv_last_val[channel] = 0;
+	return pv_val[channel];
 }
 
 // Private
 
-void lp_peackmeter::pv_start_clip()
+void lp_peackmeter::pv_start_clip(int channel)
 {
-	gettimeofday(&pv_clip_start, 0);
-	pv_clip_to_show = TRUE;
+	if(channel >= pv_channels){
+		std::cerr << "lp_peackmeter::pv_start_clip: channel value is to high\n";
+		return;
+	}
+
+	gettimeofday(&pv_clip_start[channel], 0);
+	pv_clip_to_show[channel] = TRUE;
 }
 
-bool lp_peackmeter::pv_clip_is_timeout()
+bool lp_peackmeter::pv_clip_is_timeout(int channel)
 {
+	if(channel >= pv_channels){
+		std::cerr << "lp_peackmeter::pv_clip_is_timeout: channel value is to high\n";
+		return -1;
+	}
+
 	struct timeval curent_time;
 	gettimeofday(&curent_time, 0);
 
-	if((curent_time.tv_usec + (curent_time.tv_sec * 1000000)) - (pv_clip_start.tv_usec + (pv_clip_start.tv_sec * 1000000)) >= pv_clip_timeout) {
+	if((curent_time.tv_usec + (curent_time.tv_sec * 1000000)) - (pv_clip_start[channel].tv_usec + (pv_clip_start[channel].tv_sec * 1000000)) >= pv_clip_timeout) {
 		return TRUE;
 	}else{
 		return FALSE;
@@ -255,14 +257,20 @@ bool lp_peackmeter::pv_clip_is_timeout()
 
 lp_peackmeter_dlg::lp_peackmeter_dlg(QWidget *parent, const char *name, lp_peackmeter *pm) : QWidget(parent, name)
 {
+	int i;
 	pv_peack_meter = pm;
 
 	pv_channels = pm->get_channels();
+	for(i=0; i<pv_channels; i++){
+		pv_lb[i] = 0;
+	}
 
-	pv_bl = new QBoxLayout(this, QBoxLayout::LeftToRight, 5, 10);
+	pv_bl = new QBoxLayout(this, QBoxLayout::LeftToRight, -1, 0, "lp_peackmeter_dlg layout");
+	set_meter_size(3);
 	// Widget size
-	setMinimumHeight(250);
-	setMinimumWidth(50 * pv_channels);
+	//setMinimumWidth(40 * pv_channels);
+	//setMaximumWidth(40 * pv_channels);
+	//pv_bl->setMargin(0);
 
 	// Build widgets
 	if(pv_build_widgets() < 0){
@@ -270,14 +278,12 @@ lp_peackmeter_dlg::lp_peackmeter_dlg(QWidget *parent, const char *name, lp_peack
 		return;
 	}
 
-	int i;
 	if((pv_channels >= 1)||(pv_channels <= MAX_CHANNELS)){
 		for(i=0; i<pv_channels; i++){
-			draw_meter(pv_lb[i], 0);
+			draw_meter(pv_lb[i], 0, i);
+			pv_clip[i] = TRUE;
 		}
 	}
-
-	pv_clip = TRUE;
 
 	// Down factors
 	pv_down_ref_time = 1500000;
@@ -295,6 +301,31 @@ lp_peackmeter_dlg::~lp_peackmeter_dlg()
 
 }
 
+void lp_peackmeter_dlg::set_meter_size(int size)
+{
+	if(size == 1){
+		pv_meter_height = 100;
+		//setMinimumHeight(250);
+	}else if(size == 2){
+		pv_meter_height = 200;
+		//setMinimumHeight(250);
+	}else if(size == 3){
+		pv_meter_height = 300;
+	}else{
+		std::cerr << "lp_peackmeter_dlg::set_meter_size: size is not in range (aviable: 1, 2 or 3)\n";
+		std::cerr << "\t-> Chosen value: 2 (medium)\n";
+		pv_meter_height = 200;
+	}
+	// Resize the meter
+	int i;
+	for(i=0; i<pv_channels; i++){
+		if(pv_lb[i] != 0){
+			pv_lb[i]->setMinimumHeight(pv_meter_height + 30);
+			pv_lb[i]->setMaximumHeight(pv_meter_height + 30);
+		}
+	}
+}
+
 // Protected
 void lp_peackmeter_dlg::paintEvent(QPaintEvent*)
 {
@@ -308,7 +339,7 @@ void lp_peackmeter_dlg::paintEvent(QPaintEvent*)
 	// Draw
 	int i;
 	for(i=0; i<pv_channels; i++){
-		draw_meter(pv_lb[i], 0);
+		draw_meter(pv_lb[i], 0, i);
 	}
 }
 
@@ -326,9 +357,8 @@ void lp_peackmeter_dlg::customEvent(QCustomEvent *event)
 		// Draw
 		int i;
 		for(i=0; i<pv_channels; i++){
-			draw_meter(pv_lb[i], t_event->val);
-			pv_txt[i]->setNum(t_event->txt_val);
-		//	std::cout << "Val: " << t_event->val << " , txt_val: " << t_event->txt_val << std::endl;
+			draw_meter(pv_lb[i], t_event->val[i], i);
+			pv_txt[i]->setNum(t_event->txt_val[i]);
 		}
 	}else{
 		QWidget::customEvent(event);
@@ -337,23 +367,32 @@ void lp_peackmeter_dlg::customEvent(QCustomEvent *event)
 
 void lp_peackmeter_dlg::run()
 {
-	float val, old_val, peack_hold_val;
+	// Verification
+	if((pv_channels < 1)||(pv_channels >= MAX_CHANNELS)) {
+		std::cerr << "lp_peackmeter_dlg::run: pv_channels is not in range\n";
+		std::cerr << "\tGiven value: " << pv_channels << " , range from 1 to " << MAX_CHANNELS << std::endl;
+		return;
+	}	
+
+	float val[MAX_CHANNELS], old_val[MAX_CHANNELS], peack_hold_val[MAX_CHANNELS];
+	int i;
 	struct timeval t0, t1;
 	gettimeofday(&t0, 0);
 
 	while(1){
-		if(pv_peack_meter->get_clip() == TRUE){
-	//		std::cout << "###### CLIP ! #######\n";
-			pv_clip = TRUE;
-		}else{
-			pv_clip = FALSE;
+		for(i=0; i<pv_channels; i++){
+			if(pv_peack_meter->get_clip(i) == TRUE){
+		//		std::cout << "###### CLIP ! #######\n";
+				pv_clip[i] = TRUE;
+			}else{
+				pv_clip[i] = FALSE;
+			}
+			val[i] = pv_peack_meter->get_value(i);
+	
+			val[i] = pv_down_val(val[i], i);
+			peack_hold_val[i] = pv_peack_hold(val[i], i);
+	
 		}
-		val = pv_peack_meter->get_value();
-
-		val = pv_down_val(val);
-		peack_hold_val = pv_peack_hold(val);
-
-		//std::cout << "VAL: " << val << "\n";
 		// envois toutes les 1/25 sec (=0.04s --> 40ms --> 40000us)
 		// Send a vu_event every 1/25 seconds (=0.04s --> 40ms --> 40000us)
 		gettimeofday(&t1, 0);
@@ -363,40 +402,48 @@ void lp_peackmeter_dlg::run()
 			vu_event *event = new vu_event;
 
 			// Traitement ICI
-			event->val = pv_val_to_db(val);
-			event->txt_val = pv_val_to_db(peack_hold_val);
+			for(i=0; i<pv_channels; i++){
+				event->val[i] = pv_val_to_db(val[i]);
+				event->txt_val[i] = pv_val_to_db(peack_hold_val[i]);
+				old_val[i] = val[i];
+			}
 			QApplication::postEvent(this, event);
-			old_val = val;
 		}
-
 		usleep(100);
 	}
 
 }
 
-void lp_peackmeter_dlg::draw_meter(QPaintDevice *dev, int val)
+void lp_peackmeter_dlg::draw_meter(QPaintDevice *dev, int val, int channel)
 {
+	// Verification
+	if((pv_channels < 1)||(pv_channels >= MAX_CHANNELS)) {
+		std::cerr << "lp_peackmeter_dlg::draw_meter: pv_channels is not in range\n";
+		std::cerr << "\tGiven value: " << pv_channels << " , range from 1 to " << MAX_CHANNELS << std::endl;
+		return;
+	}	
+
 	QPainter paint(dev);
 	QRect dim = paint.window();
 	// nettoyer
 	paint.setPen(QPen(Qt::white));
 	paint.setBrush(QBrush(Qt::white));
-	paint.drawRect(10, 250, 10, -200);
+	paint.drawRect(10, pv_meter_height + 20, 10, -pv_meter_height);
 
-	paint.setPen(QPen(Qt::blue));
-	paint.setBrush(QBrush(Qt::blue));
 	// clip
-	if(pv_clip == TRUE){
+	if(pv_clip[channel] == TRUE){
 		paint.setBrush(QBrush(Qt::red));
 		paint.drawEllipse(10, 5, 10, 10);
-		paint.setBrush(QBrush(Qt::blue));
-		pv_clip = FALSE;
+		paint.setBrush(QBrush(Qt::green));
+		pv_clip[channel] = FALSE;
 	}else{
 		paint.setBrush(QBrush(Qt::blue));
 		paint.drawEllipse(10, 5, 10, 10);
 	}
-	paint.drawRect(10, 250, 10, -(pv_iec_scale((float)val)));
-//	std::cout << "Draw: " <<  -(pv_iec_scale((float)val)) << std::endl;
+	// Bar
+	paint.setPen(QPen(Qt::green));
+	paint.setBrush(QBrush(Qt::green));
+	paint.drawRect(10, pv_meter_height + 20, 10, -(pv_iec_scale((float)val)));
 }
 
 // Private functions
@@ -423,7 +470,8 @@ int lp_peackmeter_dlg::pv_iec_scale(float db) {
                  def = 100.0f;
          }
  
-         return (int)(def * 2.0f);
+         //return (int)(def * 2.0f);
+	return (int)(def * (float)pv_meter_height/100);
 }
 
 int lp_peackmeter_dlg::pv_val_to_db(float f_val)
@@ -431,8 +479,15 @@ int lp_peackmeter_dlg::pv_val_to_db(float f_val)
 	return (int)(20*((float)log10(f_val)));
 }
 
-float lp_peackmeter_dlg::pv_down_val(float act_val)
+float lp_peackmeter_dlg::pv_down_val(float act_val, int channel)
 {
+	// Verification
+	if((pv_channels < 1)||(pv_channels >= MAX_CHANNELS)) {
+		std::cerr << "lp_peackmeter_dlg::pv_down_val: pv_channels is not in range\n";
+		std::cerr << "\tGiven value: " << pv_channels << " , range from 1 to " << MAX_CHANNELS << std::endl;
+		return -1;
+	}	
+
 	struct timeval actual_time;
 	gettimeofday(&actual_time, 0);
 
@@ -440,33 +495,40 @@ float lp_peackmeter_dlg::pv_down_val(float act_val)
 	actual_usec = actual_time.tv_usec + (actual_time.tv_sec * 1000000);
 
 	unsigned int last_usec;		// last time in usec
-	last_usec = pv_last_time.tv_usec + (pv_last_time.tv_sec * 1000000);
+	last_usec = pv_last_time[channel].tv_usec + (pv_last_time[channel].tv_sec * 1000000);
 
 	unsigned int delta_t;
 	delta_t = actual_usec - last_usec;
 
 	float tmp_val;
 
-	if(act_val < pv_last_val){
-		tmp_val = pv_last_peack * ((pv_down_factor * pv_down_ref_time) / delta_t );
+	if(act_val < pv_last_val[channel]){
+		tmp_val = pv_last_peack[channel] * ((pv_down_factor * pv_down_ref_time) / delta_t );
 
-		if(tmp_val < pv_last_val){
-			pv_last_val = tmp_val;
+		if(tmp_val < pv_last_val[channel]){
+			pv_last_val[channel] = tmp_val;
 		}
-		return pv_last_val;
+		return pv_last_val[channel];
 	}else{
 		// reset timer
 //		std::cout << "PEAK - Reset timer ###################################################\n";
-		pv_last_peack = act_val;
-		pv_last_val = act_val;
-		gettimeofday(&pv_last_time, 0);
+		pv_last_peack[channel] = act_val;
+		pv_last_val[channel] = act_val;
+		gettimeofday(&pv_last_time[channel], 0);
 	}
 
 	return act_val;
 }
 
-float lp_peackmeter_dlg::pv_peack_hold(float act_val)
+float lp_peackmeter_dlg::pv_peack_hold(float act_val, int channel)
 {
+	// Verification
+	if((pv_channels < 1)||(pv_channels >= MAX_CHANNELS)) {
+		std::cerr << "lp_peackmeter_dlg::pv_peack_hold: pv_channels is not in range\n";
+		std::cerr << "\tGiven value: " << pv_channels << " , range from 1 to " << MAX_CHANNELS << std::endl;
+		return -1;
+	}	
+
 	struct timeval actual_time;
 	gettimeofday(&actual_time, 0);
 
@@ -474,7 +536,7 @@ float lp_peackmeter_dlg::pv_peack_hold(float act_val)
 	actual_usec = actual_time.tv_usec + (actual_time.tv_sec * 1000000);
 
 	unsigned int last_usec;		// last time in usec
-	last_usec = pv_last_hold_time.tv_usec + (pv_last_hold_time.tv_sec * 1000000);
+	last_usec = pv_last_hold_time[channel].tv_usec + (pv_last_hold_time[channel].tv_sec * 1000000);
 
 	unsigned int delta_t;
 	delta_t = actual_usec - last_usec;
@@ -483,11 +545,11 @@ float lp_peackmeter_dlg::pv_peack_hold(float act_val)
 
 	if(delta_t >= pv_hold_ref_time){
 		tmp_val = act_val;
-		gettimeofday(&pv_last_hold_time, 0);
-		pv_last_hold_peack = act_val;
+		gettimeofday(&pv_last_hold_time[channel], 0);
+		pv_last_hold_peack[channel] = act_val;
 	//	std::cout << "HOLD (Reset): return " << tmp_val << std::endl;
 	}else{
-		tmp_val = pv_last_hold_peack;
+		tmp_val = pv_last_hold_peack[channel];
 	//	std::cout << "HOLD: return " << tmp_val << std::endl;
 	}
 
@@ -506,16 +568,19 @@ int lp_peackmeter_dlg::pv_build_widgets()
 	// Build
 	int i;
 	for(i=0; i<pv_channels; i++){
-		pv_hbl[i] = new QBoxLayout(this, QBoxLayout::TopToBottom, 5, 10);
+		pv_hbl[i] = new QBoxLayout(this, QBoxLayout::TopToBottom, 2, 2);
 		pv_lb[i] = new QLabel(this);
-		pv_lb[i]->setBackgroundColor(Qt::green);
-		pv_lb[i]->setMinimumHeight(300);
+		pv_lb[i]->setBackgroundColor(Qt::black);
+		pv_lb[i]->setMinimumHeight(pv_meter_height + 30);
+		pv_lb[i]->setMaximumHeight(pv_meter_height + 30);
+		pv_lb[i]->setMinimumWidth(30);
+		pv_lb[i]->setMaximumWidth(30);
 		pv_hbl[i]->addWidget(pv_lb[i], -1, 0);
 		// Text label
 		pv_txt[i] = new QLabel(this);
 		pv_txt[i]->setAlignment(Qt::AlignHCenter);
 		pv_txt[i]->setText("0");
-		pv_txt[i]->setBackgroundColor(Qt::green);
+		//pv_txt[i]->setBackgroundColor(Qt::green);
 		pv_txt[i]->setMaximumHeight(30);
 		pv_hbl[i]->addWidget(pv_txt[i], -1, 0);
 		// Add to horizontal layout
--- trunk/src/lp_peackmeter/lp_peackmeter.h
+++ trunk/src/lp_peackmeter/lp_peackmeter.h
@@ -13,17 +13,29 @@
 class QBoxLayout;
 class lp_peackmeter_dlg;
 
-/// This class is to instanciate for use
+/** This class is to instanciate for use
+    Please get the instance from the main thread (GUI thread)
+    After this, you cann call the public function from another thread
+    NOTE: you should test this..
+**/
+
 class lp_peackmeter : public QThread, QWidget
 {
 	public:
+	/// Here is the constructor to use for instanciation
+	/// Give your widget as argument to put the peack-meter on it
 	lp_peackmeter( QWidget *parent = 0, const char *name = 0, int nb_channels = 1, int buf_size = 0);
 	~lp_peackmeter();
-	int run_buffer(float *buffer, int len);
-	bool get_clip();
-	float get_value();
-	int get_channels();
+	/// Use this to set / change the samplerate
 	int set_samplerate(int samplerate = 0);	// To change / set samplerate
+	/// Use this function to run the samples
+	int run_buffer(float *buffer, int len);
+
+	// These are specific functions called by lp_peackmeter_dlg
+	// for interactions. Please do not use tis functions
+	bool get_clip(int channel);
+	float get_value(int channel);
+	int get_channels();
 
 	protected:
 	virtual void run();
@@ -33,33 +45,34 @@ class lp_peackmeter : public QThread, QWidget
 	static const int MAX_CHANNELS = 20;
 
 	// Audio
-	float pv_range_low;	// For range ( 0.001 for example)
-	float pv_range_high;	// For range ( 1.0 for example)
-	float pv_val[MAX_CHANNELS];		// Instant value
-	float pv_last_val[MAX_CHANNELS];	// Store last value (store peakcs)
-	int pv_s_rate;		// Audio samplerate
-	int pv_buf_size;	// Buffer size for allocation
-	int pv_buf_len;		// Buffer len (can call smaller size than buf_len)
-	float *pv_buffer;
+	float pv_range_low;		// For range ( 0.001 for example)
+	float pv_range_high;		// For range ( 1.0 for example)
+	float pv_val[MAX_CHANNELS];	// Instant value
+	float pv_last_val[MAX_CHANNELS];// Store last value (store peaks)
+	int pv_s_rate;			// Audio samplerate
+	int pv_buf_size;		// Buffer size for allocation
+	int pv_buf_len;			// Buffer len (can call smaller size than buf_len)
+	float *pv_buffer;		// Stores the audio samples
 	int pv_channels;
 
 	// Timer
-	//unsigned int pv_buffer_time;
-	unsigned int pv_sample_time;
-	unsigned int pv_minimal_sample_time;	// Have speed problems. With to low sleep_time, the clipping works bad
-	struct timeval pv_clip_start;		// Stores the start time of clipping
-	void pv_start_clip();			// Start clip timer (stores current time in pv_clip_start)
-	int pv_clip_timeout;			// Store the clip timeout time (in usec)
-	bool pv_clip_to_show;			// Store clip status: after detection, if timeout AND showed, it goes FALSE
-	bool pv_clip_is_timeout();		// Returns TRUE if timeout is reached
+	unsigned int pv_sample_time;			// Time to sleep after each samples
+	unsigned int pv_minimal_sample_time;		// To store the minimal sample_time (I use 1us)
 
-	bool pv_buf_end;			// Whenn new call, reset process and run new buffer
-	bool pv_running;			// Whenn new call and process is not properly aborted: wait
+	// Clip detection
+	struct timeval pv_clip_start[MAX_CHANNELS];	// Stores the start time of clipping
+	void pv_start_clip(int channel);		// Start clip timer (stores current time in pv_clip_start)
+	int pv_clip_timeout;				// Store the clip timeout time (in usec)
+	bool pv_clip_to_show[MAX_CHANNELS];		// Store clip status: after detection, if timeout AND showed, it goes FALSE
+	bool pv_clip_is_timeout(int channel);		// Returns TRUE if timeout is reached
+
+	bool pv_buf_end;				// Whenn new call, reset process and run new buffer
+	bool pv_running;				// Whenn new call and process is not properly aborted: wait
 	QWaitCondition pv_qwc;
 	QMutex pv_mutex;
 	QMutex pv_dlg_mutex;
 
-	lp_peackmeter_dlg *pv_peack_meter_dlg;
+	lp_peackmeter_dlg *pv_peack_meter_dlg;		// Store the dlg instance here
 };
 
 // This is used from lp_peackmeter class
@@ -71,6 +84,9 @@ class lp_peackmeter_dlg : public QWidget, QThread
 	lp_peackmeter_dlg(QWidget *parent = 0, const char *name = 0, lp_peackmeter *pm = 0);
 	~lp_peackmeter_dlg();
 
+	// Set meter size: 1 = little, 2 = medium, 3 = big
+	void set_meter_size(int size);
+
 	protected:
 	virtual void paintEvent(QPaintEvent*);
 	virtual void customEvent(QCustomEvent *event);
@@ -81,49 +97,60 @@ class lp_peackmeter_dlg : public QWidget, QThread
 	static const int MAX_CHANNELS = 20;
 
 	int pv_channels;
-	bool pv_clip;
 
-	// Convert a float value to dB val
-	int pv_val_to_db(float f_val);
+	// Clip display
+	bool pv_clip[MAX_CHANNELS];
+
+	int pv_val_to_db(float f_val);		// Convert a float value to dB val
+
 	// Calculate a instant value depend on time (peack_meter goes down with 55dB/1.5 sec in std)
-	float pv_down_val(float act_val);
-	float pv_peack_hold(float act_val);
+	float pv_down_val(float act_val, int channel);
+	float pv_down_factor;				// down factor: -55dB in std -> pow(10, (-55/20))
+	unsigned int pv_down_ref_time;			// ref. time: 1.5sec in std -> 1 500 000us
+	struct timeval pv_last_time[MAX_CHANNELS];	// elapsed time at last peack
+	float pv_last_peack[MAX_CHANNELS];		// Last peack value
+	float pv_last_val[MAX_CHANNELS];		// Store previous value
 
-	unsigned int pv_down_ref_time;	// ref. time: 1.5sec in std -> 1 500 000us
-	unsigned int pv_hold_ref_time;	// ref. for peack hold
-	struct timeval pv_last_time;	// elapsed time at last peack
-	struct timeval pv_last_hold_time; // elapsed time at last peack - for peack hold
-	float pv_down_factor;		// down factor: -55dB in std -> pow(10, (-55/20))
-	float pv_last_peack;		// Last peack value
-	float pv_last_hold_peack;	// last value for peack hold
-	float pv_last_val;		// Store previous value
+	// Import from meterbridge sources (http://plugin.org.uk/meterbridge/)
+	int pv_iec_scale(float db);			// Give the meter the scale
 
-	lp_peackmeter *pv_peack_meter;		// The peack_meter instance
+	// Hold a value for some time (Used to display numerical values slow, else there ares illisible)
+	float pv_peack_hold(float act_val, int channel);
+	float pv_last_hold_peack[MAX_CHANNELS];		// last value for peack hold
+	unsigned int pv_hold_ref_time;			// ref. for peack hold
+	struct timeval pv_last_hold_time[MAX_CHANNELS]; // elapsed time at last peack - for peack hold
+
+	lp_peackmeter *pv_peack_meter;			// The peack_meter instance
 
 	// Qt objects
-	QBoxLayout *pv_bl;
-	QBoxLayout *pv_hbl[MAX_CHANNELS];
-	QLabel *pv_lb[MAX_CHANNELS];		// The vu_meter
-	QLabel *pv_txt[MAX_CHANNELS];		// The text
+	QBoxLayout *pv_bl;				// Main box layout
+	QBoxLayout *pv_hbl[MAX_CHANNELS];		// Horizontal box layout
+	QLabel *pv_lb[MAX_CHANNELS];			// The vu_meters
+	QLabel *pv_txt[MAX_CHANNELS];			// The texts
 	QWaitCondition pv_qwc;
 	QMutex pv_mutex;
+	QMutex pv_cfg_mutex;
 
-	void draw_meter(QPaintDevice *dev, int val);
+	// Dimentions
+	int pv_meter_height;		// The "bargraph" height
 
 	// Build widgets
 	int pv_build_widgets();
 
-	// Tests: import from meterbridge sources
-	int pv_iec_scale(float db);
+	void draw_meter(QPaintDevice *dev, int val, int channel);
 };
 
 // The event class to transmission between lp_peackmeter_dlg's run thread and main thread
 class vu_event : public QCustomEvent
 {
+	private:
+	// some constants
+	static const int MAX_CHANNELS = 20;
+
 	public:
 	vu_event();
-	int val;
-	int txt_val;
+	int val[MAX_CHANNELS];
+	int txt_val[MAX_CHANNELS];
 };
 
 #endif // LP_PEAKMETER_H
